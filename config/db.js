1a3b0fcf134df1057f8d41af76e51a58d75b6baa
Kem Heng <KemHeng>
2023-08-31 00:07:44 -0700

    first commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
 
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database : process.env.DB_DBNAME,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database : "personal_blog"
 
 }); 
 
