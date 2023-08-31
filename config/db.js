92c851f32ca4487af35876e740ad94c5dc14f24c
Kem Heng <KemHeng>
2023-08-31 04:13:50 -0700

    updateBLOG

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
 
-  host: "localhost",
-  user: "root",
-  password: "",
-  database : "personal_blog"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database : process.env.DB_DBNAME,
 
 }); 
 
